ac980829906e6d53411b92ccfee44116eb716398
孟祥建 <mengxiangjian@letv.com>
2017-01-07 21:24:02 +0800

    add gitignore file

---
+++ LinkMapParser/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  LinkMapParser
+//
+//  Created by mengxiangjian on 2017/1/7.
+//  Copyright © 2017年 mengxiangjian. All rights reserved.
+//
+
+import Foundation
---
+++ LinkMapParser/SreamReader.swift
@@ -0,0 +1,9 @@
+//
+//  SreamReader.swift
+//  LinkMapParser
+//
+//  Created by mengxiangjian on 2017/1/7.
+//  Copyright © 2017年 mengxiangjian. All rights reserved.
+//
+
+import Foundation
